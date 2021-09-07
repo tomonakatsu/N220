14d5dcf67e113e652aef5ddaf7b30d4867a71c3b
Nakatsuji, Tomoki <tnakatsu@iu.edu>
2021-09-07 18:57:15 -0400

    changed variables' name

--- AaCOE/4.Ball Bounce/BB.js
+++ AaCOE/4.Ball Bounce/BB.js
@@ -1,7 +1,7 @@
 let sqSize = 75;
 let sqColor = "#a1eb46";
-let xvelocity = 5;
-let yvelocity = 5;
+let xPos = 5;
+let yPos = 5;
 let x = 400;
 let y = 300;
 
@@ -13,13 +13,13 @@ function draw() {
   background(0);
   fill(sqColor);
   circle(x, y, sqSize);
-  x += xvelocity;
-  y += yvelocity;
+  x += xPos;
+  y += yPos;
 
   if (x < 0 || x > 800){
-    xvelocity *= -1;
+    xPos *= -1;
   }
   if (y < 0 || y > 600){
-    yvelocity *= -1;
+    yPos *= -1;
   }
 }
