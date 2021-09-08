e6358a83bc0e29a14946402ac7a7279eda2fd0a6
Nakatsuji, Tomoki <tnakatsu@iu.edu>
2021-09-08 12:40:57 -0400

    added lab3

---
+++ lab3/lab3.js
@@ -0,0 +1,16 @@
+let sqSize = 50;
+let sqColor = "#FFA500";
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+
+  fill(sqColor);
+  x = width-mouseX;
+  y = height-mouseY;
+  circle(x, y, sqSize);
+
+}
