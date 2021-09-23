16a6ac6aefaa792b9f4667d4289bfb1266356ef8
Nakatsuji, Tomoki <tnakatsu@iu.edu>
2021-09-23 16:02:31 -0400

    nvm

--- Arrays Exercises/3.Click to drop/Ctd.js
+++ Arrays Exercises/3.Click to drop/Ctd.js
@@ -9,6 +9,11 @@ function setup(){
     createCanvas(400, 400);
 }
 
+function mousePressed(){
+    x.push(mouseX);
+    y.push(mouseY);
+}
+
 function draw(){
     background(0);
     for(let i = 0; i < x.length; i++){
@@ -16,9 +21,4 @@ function draw(){
         rect(x[i] - brWidth/2, y[i] - brHeight/2, brWidth, brHeight)
         y[i]+= yPos;
     }
-}
-
-function mousePressed(){
-    x.push(mouseX);
-    y.push(mouseY);
 }
