1b790c359710bc1d1eb7f61a3a055f1fe508b2bd
Nakatsuji, Tomoki <tnakatsu@iu.edu>
2021-10-05 18:56:56 -0400

    added 10/5 class exercise

---
+++ Lab Exercises/Class 10_5/10_5.js
@@ -0,0 +1,52 @@
+let meeples = [];
+var job = ["teacher", "doctor", "pilot", "none"];
+let meeplesId = document.getElementById("meeples");
+
+for(let i = 0; i < 20; i++) {
+  meeples.push( { name: "Meeple #"+i, age: Math.floor(Math.random() * 50), 
+                job: job[Math.floor(Math.random()*job.length)]});
+    console.log(meeples[i]);
+}
+
+console.log("");
+
+function age(){
+    for(let i = 0; i < meeples.length; i++) {
+        meeples[i].age ++;
+        if (meeples[i].age > 18 & meeples[i].job == "none"){
+            meeples[i].job = job[Math.floor(Math.random()*2)];
+        }
+        if(meeples[i].age >= 55){
+            chance = Math.random();
+            if(chance > 0.5){
+                meeples.splice(i, 1);
+            }
+        }
+        console.log(meeples[i]);
+    }
+    console.log(" ");
+}
+
+function show(){
+    for(let i = 0; i < meeples.length ; i++){
+        document.write(meeples[i] + "<br >");
+    }
+}
+//log out the contents of meeples.. what is in it?
+
+//When a button is pressed
+    // loop through the meeples
+    //add one to their age
+    //if their age is greater than 18 and they don't have a job..
+         //set their job to (randomly) teacher, doctor, pilot
+    //if their age is greater than 55
+          // on a 50% chance
+          // remove them from the array
+
+//When a different button is pressed
+   // clear out old data
+    // write the content ofthe array to the screen
+
+//when a  third button is pressed
+
+  // add a new person to the meeples array
