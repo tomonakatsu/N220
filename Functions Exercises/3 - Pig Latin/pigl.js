8360531e19538026431143b12490c6f027144438
Nakatsuji, Tomoki <tnakatsu@iu.edu>
2021-11-02 19:57:30 -0400

    updated lab 8 index

--- Functions Exercises/3 - Pig Latin/pigl.js
+++ Functions Exercises/3 - Pig Latin/pigl.js
@@ -8,17 +8,15 @@ function doThing(){
 }
 
 function checker(input){
-    let char = [];
     let counter = 0;
 
     for(let i = 0; i < input.length; i++) {
         //check if the letter is vowel
-        if(input[i] != "a" && input[i] != "e" && input[i] != "i" && input[i] != "o" && input[i] != "u" && input[i] != "A" && input[i] != "E" && input[i] != "I" && input[i] != "O" && input[i] != "U"){
-            //
-            char = input[i];
-            
-            input = input + char;
-            //number of stirng that need to be removed from the input
+        if(input[i] != "a" && input[i] != "e" && input[i] != "i" && input[i] != "o" && input[i] != "u" 
+        && input[i] != "A" && input[i] != "E" && input[i] != "I" && input[i] != "O" && input[i] != "U"){
+  
+            input = input + input[i];
+            //to count the number of stirng that need to be removed from the input
             counter ++;
         }
         else{
