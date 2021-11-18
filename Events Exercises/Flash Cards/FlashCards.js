b855b2ef76eb29b18604350f7f4e7cb7ec361979
Nakatsuji, Tomoki <tnakatsu@iu.edu>
2021-11-18 17:16:13 -0500

    od -> of (correct text error)

--- Events Exercises/Flash Cards/FlashCards.js
+++ Events Exercises/Flash Cards/FlashCards.js
@@ -1,11 +1,11 @@
 let message = document.getElementById("message");
 
 function doThing(event){
-    let answer = event.target.getAttribute("data-response");
+    let answer = event.target.getAttribute("data-answer");
     if(answer == "correct"){
-        message.innerHTML = "Correct! The capital od Indiana is Indianapolis";
+        message.innerHTML = "Correct! The capital of Indiana is Indianapolis";
     }
     else{
-        message.innerHTML = "Incorrect! The capital od Indiana is Indianapolis";
+        message.innerHTML = "Incorrect! The capital of Indiana is Indianapolis";
     }
 }
