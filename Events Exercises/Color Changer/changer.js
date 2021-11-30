e35e867699c79af7f538237beaad855913b32ae0
Nakatsuji, Tomoki <tnakatsu@iu.edu>
2021-11-30 10:54:06 -0500

    made it more simple

--- Events Exercises/Color Changer/changer.js
+++ Events Exercises/Color Changer/changer.js
@@ -1,29 +1,19 @@
 let first = document.getElementById("first");
 let second = document.getElementById("second");
 let third = document.getElementById("third");
-console.log(first);
+let order = [first, second, third];
 
 let size = 200;
 let margin = 5;
 let color = "#808080";
 
-first.style.height = size + "px";
-first.style.width = size + "px";
-first.style.backgroundColor = color;
-first.style.float = "left";
-first.style.margin = margin + "px";
-
-second.style.height = size + "px";
-second.style.width = size + "px";
-second.style.backgroundColor = color;
-second.style.float = "left";
-second.style.margin = margin + "px";
-
-third.style.height = size + "px";
-third.style.width = size + "px";
-third.style.backgroundColor = color;
-third.style.float = "left";
-third.style.margin = margin + "px";
+for(let i = 0; i < order.length; i++){
+    order[i].style.height = size + "px";
+    order[i].style.width = size + "px";
+    order[i].style.backgroundColor = color;
+    order[i].style.float = "left";
+    order[i].style.margin = margin + "px";
+}
 
 function doThing(event){
     let ind_color = event.target.getAttribute("data-response");
